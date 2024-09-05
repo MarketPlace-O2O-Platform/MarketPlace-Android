0346e1999cc98b50e3e28bedbf136b4125ed71ca
바키찬 <ckstmznf@naver.com>
2024-09-05 12:14:23 +0900

    폰트 추가

--- app/src/main/java/dev/kichan/marketplace/ui/theme/Type.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/theme/Type.kt
@@ -2,9 +2,23 @@ package dev.kichan.marketplace.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.kichan.marketplace.R
+
+val PretendardFamily = FontFamily(
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_black, FontWeight.Black),
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extra_light, FontWeight.ExtraLight),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
