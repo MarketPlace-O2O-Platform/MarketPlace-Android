52dcdb44c091cfe51ecda42cb65fb673515d3c3f
바키찬 <ckstmznf@naver.com>
2024-10-29 18:19:32 +0900

    InputLable

--- app/src/main/java/dev/kichan/marketplace/ui/component/InputLable.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/component/InputLable.kt
@@ -10,6 +10,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -95,9 +98,11 @@ fun InputLabelPreview() {
 @Composable
 fun InputLabelErrorPreview() {
     MarketPlaceTheme {
+        val input = remember { mutableStateOf("") }
+
         InputLabel(
-            value = "",
-            onChange = {},
+            value = input.value,
+            onChange = { input.value = it },
             label = "이름",
             modifier = Modifier
                 .padding(8.dp)
