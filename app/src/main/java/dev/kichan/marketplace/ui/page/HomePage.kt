c994772dc2dfa041fe667ab2a04884e35b73aeec
바키찬 <ckstmznf@naver.com>
2024-11-04 23:47:40 +0900

    Feat: HomePage SearchBar 추가

--- app/src/main/java/dev/kichan/marketplace/ui/page/HomePage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/HomePage.kt
@@ -11,25 +11,31 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,10 +43,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -74,38 +83,13 @@ fun HomePage(navController: NavController) {
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            // 아이콘 상단 바
-            Row(
-                Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(onClick = { /* 검색 */ }) {
-                    Icon(
-                        Icons.Outlined.Search,
-                        contentDescription = "Search",
-                        modifier = Modifier.size(32.dp)  // 사이즈 조정
-                    )
-                }
-                Spacer(modifier = Modifier.width(10.dp))
-                IconButton(onClick = { /* 알림 */ }) {
-                    Icon(
-                        Icons.Outlined.Notifications,
-                        contentDescription = "Notifications",
-                        modifier = Modifier.size(32.dp)
-                    )
-                }
-            }
-
             LazyColumn {
+                item {
+                    SearchBar()
+                }
                 // 쿠폰 배너 바로 상단바 아래에 위치
                 item {
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                    ) {
-//                    }
+                    Spacer(modifier = Modifier.height(20.dp))
                     CouponBanner()
                 }
 
@@ -146,7 +130,8 @@ fun CouponBanner(modifier: Modifier = Modifier) {
     val pagerState = rememberPagerState(initialPage = 0, pageCount = { bannerList.size })
 
     Box(
-        Modifier.padding(horizontal = PAGE_HORIZONTAL_PADDING)
+        Modifier
+            .padding(horizontal = PAGE_HORIZONTAL_PADDING)
             .clip(shape = RoundedCornerShape(12.dp))
     ) {
         HorizontalPager(state = pagerState) {
@@ -163,7 +148,9 @@ fun CouponBanner(modifier: Modifier = Modifier) {
             }
         }
         PagerCounter(
-            modifier = Modifier.align(Alignment.BottomEnd).padding(20.dp),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(20.dp),
             pagerState = pagerState
         )
 //        PagerIndicator(
@@ -172,6 +159,68 @@ fun CouponBanner(modifier: Modifier = Modifier) {
 //                .padding(bottom = 20.dp),
 //            pagerState = pagerState,
 //        )
+    }
+}
+
+@Composable
+fun SearchBar() {
+    var searchText by remember { mutableStateOf(TextFieldValue("")) }
+
+    Box(
+        modifier = Modifier
+            .padding(horizontal = PAGE_HORIZONTAL_PADDING)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color(0xFFF5F5F5), shape = RoundedCornerShape(24.dp))
+            .padding(horizontal = 16.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                tint = Color(0xff121212),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .background(Color(0xffff0000))
+                    .width(1.dp)
+                    .fillMaxHeight()
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            BasicTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Normal),
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) { innerTextField ->
+                if (searchText.text.isEmpty()) {
+                    Text(
+                        text = "찾으시는 이용권을 검색해 보세요.",
+                        style = TextStyle(color = Color(0xffB0B0B0 )),
+                    )
+                }
+                innerTextField()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    MarketPlaceTheme {
+        SearchBar()
     }
 }
 
