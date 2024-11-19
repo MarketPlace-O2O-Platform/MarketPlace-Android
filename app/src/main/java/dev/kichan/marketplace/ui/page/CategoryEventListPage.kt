24fc26d3f180fa13014969bd07575bb322f771d4
바키찬 <ckstmznf@naver.com>
2024-11-19 15:18:45 +0900

    Fix: CategoryEventListPage Pager 추가

--- app/src/main/java/dev/kichan/marketplace/ui/page/CategoryEventListPage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/CategoryEventListPage.kt
@@ -1,6 +1,7 @@
 package dev.kichan.marketplace.ui.page
 
 import LargeCategory
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,8 +32,13 @@ import dev.kichan.marketplace.ui.component.NavAppBar
 import dev.kichan.marketplace.ui.theme.MarketPlaceTheme
 
 @Composable
-fun CategoryEventListPage(navController: NavController, category: String) {
-    var selectedCategory by remember { mutableStateOf(LargeCategory.findByNameKo(category)) }
+fun CategoryEventListPage(navController: NavController, category: LargeCategory) {
+    var selectedCategory by remember { mutableStateOf(category) }
+    val pagerState = rememberPagerState(initialPage =  LargeCategory.entries.indexOf(category), pageCount = { LargeCategory.entries.size })
+
+    LaunchedEffect(pagerState.currentPage) {
+        selectedCategory = LargeCategory.entries[pagerState.currentPage]
+    }
 
     Scaffold(
         topBar = {
@@ -40,28 +49,30 @@ fun CategoryEventListPage(navController: NavController, category: String) {
     ) {
         Column(Modifier.padding(it)) {
             CategoryTap(selectedCategory = selectedCategory, onSelected = { selectedCategory = it })
-            LazyColumn(
-                contentPadding = PaddingValues(16.dp),
-            ) {
-                items(50) {
-                    EventListItem(
-                        imageRes = R.drawable.desert,
-                        title = "참피온삼겹살 트리플 스트리트점",
-                        couponDescription = "방탈출 카페 2인 이용권\n스머프와 함께 즐기는 미디어아트 보드게임!",
-                        location = "송도",
-                        likes = 440,
-                        category = "음식&주점",
-                        modifier = Modifier
-                    )
+            HorizontalPager(state = pagerState) {
+                LazyColumn(
+                    contentPadding = PaddingValues(16.dp),
+                ) {
+                    items(50) {
+                        EventListItem(
+                            imageRes = R.drawable.desert,
+                            title = "참피온삼겹살 트리플 스트리트점",
+                            couponDescription = "방탈출 카페 2인 이용권\n스머프와 함께 즐기는 미디어아트 보드게임!",
+                            location = "송도",
+                            likes = 440,
+                            category = "음식&주점",
+                            modifier = Modifier
+                        )
 
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Spacer(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color(0xffEAEAEA))
-                            .height(1.dp)
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color(0xffEAEAEA))
+                                .height(1.dp)
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
                 }
             }
         }
@@ -72,6 +83,6 @@ fun CategoryEventListPage(navController: NavController, category: String) {
 @Composable
 fun PopularEventPagePreview() {
     MarketPlaceTheme {
-        CategoryEventListPage(rememberNavController(), LargeCategory.Food.nameKo)
+        CategoryEventListPage(rememberNavController(), LargeCategory.Food)
     }
 }
