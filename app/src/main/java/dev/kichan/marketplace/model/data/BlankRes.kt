6af521ad63db383910067ea18e85d11f052be911
바키찬 <ckstmznf@naver.com>
2025-01-29 21:59:06 +0900

    dto reset

--- app/src/main/java/dev/kichan/marketplace/model/data/BlankRes.kt
+++
@@ -1,5 +0,0 @@
-package dev.kichan.marketplace.ui.component.dev.kichan.marketplace.model.data
-
-class BlankRes {
-
-}
--- app/src/main/java/dev/kichan/marketplace/model/data/image/ImageRes.kt
+++
@@ -1,9 +0,0 @@
-package dev.kichan.marketplace.ui.component.dev.kichan.marketplace.model.data.image
-
-import com.google.gson.annotations.SerializedName
-
-data class ImageRes(
-    @SerializedName("imageId") val id: Long,
-    val sequence: Int,
-    val name: String
-)
