441c264a698ea5fc2f50ebb1119711a8ce117c2e
바키찬 <ckstmznf@naver.com>
2025-01-30 21:21:52 +0900

    Feat: CouponUserService

---
+++ app/src/main/java/dev/kichan/marketplace/model/service/CouponUserService.kt
@@ -0,0 +1,37 @@
+package dev.kichan.marketplace.model.service
+
+import dev.kichan.marketplace.model.data.ResponseTemplate
+import dev.kichan.marketplace.model.data.coupon.CouponHandleRes
+import dev.kichan.marketplace.model.data.coupon.CouponPagenation
+import dev.kichan.marketplace.model.data.coupon.IssuedCouponRes
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface CouponUserService {
+    @PUT("/api/members/coupons")
+    suspend fun useCoupon(
+        @Query("memberCouponId") memberCouponId: Long
+    ) : Response<ResponseTemplate<CouponHandleRes>>
+
+    @POST("/api/members/coupons/{couponId}}")
+    suspend fun createUserCoupon(
+        @Query("memberId") memberId: Long,
+        @Query("couponId") couponId: Long,
+    )
+
+    @GET("/api/members/coupons/{memberCouponId}")
+    suspend fun getMemberCoupon(
+        @Query("memberCouponId") memberCouponId: Long
+    ) : Response<ResponseTemplate<IssuedCouponRes>>
+
+    @GET("/api/members/coupons/valid")
+    suspend fun getMemberCoupons(
+        @Query("memberId") memberId: Long,
+        @Query("type") type: String?,
+        @Query("memberCouponId") lastMemberCouponId: Long?,
+        @Query("size") pageSize: Int?
+    ) : Response<ResponseTemplate<CouponPagenation<IssuedCouponRes>>>
+}
