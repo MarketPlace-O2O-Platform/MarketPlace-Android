4480a7405d8390a3e98478160b950deabfa4c0cc
kihaeu <163748800+kihaeu@users.noreply.github.com>
2025-02-19 16:59:07 +0900

    Complement Request.kt

--- app/src/main/java/dev/kichan/marketplace/ui/page/SearchPage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/SearchPage.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -90,7 +89,7 @@ fun MarketplaceUI() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(2) {
+            items(4) {
                 BenefitCard()
             }
         }
