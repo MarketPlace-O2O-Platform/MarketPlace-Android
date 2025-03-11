c562d821c338011e83c2da3a0f40b7528a5bf160
바키찬 <ckstmznf@naver.com>
2025-03-11 21:30:07 +0900

    Feat:SearchPage SearchBar

--- app/src/main/java/dev/kichan/marketplace/ui/page/SearchPage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/SearchPage.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,108 +29,132 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.kichan.marketplace.R
+import dev.kichan.marketplace.ui.theme.MarketPlaceTheme
 import dev.kichan.marketplace.ui.theme.PretendardFamily
 
 @Composable
-fun MarketplaceUI() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(56.dp)
-            .padding(vertical=12.dp)
-            .padding(start = 9.dp, end=16.dp),
-            verticalAlignment = Alignment.CenterVertically
+fun SearchPage(modifier: Modifier = Modifier) {
+    var key by remember { mutableStateOf<String>("") }
 
-
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.left_black),
-                contentDescription = "Back",
-                modifier = Modifier
-                    .width(24.dp)
-                    .height(24.dp)
-                    .padding(1.dp)
+            SearchBar(key, { key = it })
+            Divider(color = Color(0xFF303030), thickness = 1.dp)
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "최근 검색어",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 20.dp, bottom = 8.dp)
             )
-            Spacer(modifier = Modifier.width(4.dp))
-            SearchBar()
-        }
-
-        Divider(color = Color(0xFF303030), thickness = 1.dp)
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        Text(
-            text = "최근 검색어",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 20.dp, bottom=8.dp)
-        )
-        LazyRow(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(listOf("신복관", "송쭈집", "우정소갈비", "디저트39", "헬스장", "필라테스")) { keyword ->
-                Chip(text = keyword)
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(listOf("신복관", "송쭈집", "우정소갈비", "디저트39", "헬스장", "필라테스")) { keyword ->
+                    Chip(text = keyword)
+                }
             }
-        }
-        Text(
-            text = "인기 혜택",
-            fontSize = 15.sp,
-            lineHeight = 26.sp,
-            fontFamily = PretendardFamily,
-            fontWeight = FontWeight(600),
-            color = Color(0xFF000000),
-            modifier = Modifier.padding(start = 21.dp, top=48.dp, bottom = 10.dp)
-        )
-        LazyRow(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(4) {
-                BenefitCard()
+            Text(
+                text = "인기 혜택",
+                fontSize = 15.sp,
+                lineHeight = 26.sp,
+                fontFamily = PretendardFamily,
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+                modifier = Modifier.padding(start = 21.dp, top = 48.dp, bottom = 10.dp)
+            )
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(4) {
+                    BenefitCard()
+                }
             }
         }
     }
 }
 
 @Composable
-fun SearchBar() {
+fun SearchBar(
+    value: String,
+    onChange: (String) -> Unit
+) {
     Row(
-        Modifier
+        modifier = Modifier
             .fillMaxWidth()
-            .height(32.dp)
-            .padding(end=12.dp)
-            .background(color = Color(0xFFFAFAFA), shape = RoundedCornerShape(size = 50.dp)),
+            .height(56.dp)
+            .padding(vertical = 12.dp)
+            .padding(start = 9.dp, end = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Spacer(modifier = Modifier.width(4.dp))
         Image(
-            painter = painterResource(id = R.drawable.search2),
-            contentDescription = "Search",
+            painter = painterResource(id = R.drawable.left_black),
+            contentDescription = "Back",
             modifier = Modifier
-                .padding(start = 11.dp, end = 9.dp, top = 7.dp, bottom = 7.dp)
-                .width(18.dp)
-                .height(18.dp)
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.i),
-            contentDescription = "I Icon",
-            modifier = Modifier
-                .border(width = 1.dp, color = Color(0xFFC6C6C6))
+                .width(24.dp)
+                .height(24.dp)
                 .padding(1.dp)
-                .width(0.dp)
-                .height(18.dp)
         )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = "가고 싶은 매장을 찾아보세요",
-            fontSize = 12.sp,
-            lineHeight = 26.sp,
-            fontFamily = PretendardFamily,
-            fontWeight = FontWeight(400),
-            color = Color(0xFFB0B0B0),
-        )
+        Spacer(modifier = Modifier.width(4.dp))
+
+        BasicTextField(
+            value = value,
+            onValueChange = { onChange(it) }
+        ) { innerTextField ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(32.dp)
+                    .padding(end = 12.dp)
+                    .background(
+                        color = Color(0xFFFAFAFA),
+                        shape = RoundedCornerShape(size = 50.dp)
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.width(4.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.search2),
+                    contentDescription = "Search",
+                    modifier = Modifier
+                        .padding(start = 11.dp, end = 9.dp, top = 7.dp, bottom = 7.dp)
+                        .width(18.dp)
+                        .height(18.dp)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.i),
+                    contentDescription = "I Icon",
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color(0xFFC6C6C6))
+                        .padding(1.dp)
+                        .width(0.dp)
+                        .height(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+
+                if (value.isBlank()) {
+                    Text(
+                        text = "가고 싶은 매장을 찾아보세요",
+                        fontSize = 12.sp,
+                        lineHeight = 26.sp,
+                        fontFamily = PretendardFamily,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFFB0B0B0),
+                    )
+                } else {
+                    innerTextField()
+                }
+            }
+        }
     }
 }
 
@@ -137,13 +167,15 @@ fun Chip(text: String) {
         modifier = Modifier
             .padding(start = 12.dp, top = 6.dp, bottom = 6.dp)
     ) {
-        Text(text = text,
+        Text(
+            text = text,
             fontSize = 15.sp,
             lineHeight = 21.sp,
             fontFamily = PretendardFamily,
             fontWeight = FontWeight(400),
             color = Color(0xFF5E5E5E),
-            modifier = Modifier.padding(start = 12.dp, top=6.dp, end=12.dp, bottom=6.dp))
+            modifier = Modifier.padding(start = 12.dp, top = 6.dp, end = 12.dp, bottom = 6.dp)
+        )
     }
 }
 
@@ -163,7 +195,9 @@ fun BenefitCard() {
                     painter = painterResource(id = R.drawable.brown),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.height(172.dp).fillMaxWidth()
+                    modifier = Modifier
+                        .height(172.dp)
+                        .fillMaxWidth()
                 )
                 Image(
                     painter = painterResource(id = R.drawable.bookmark2), // 스크랩 기능 아이콘
@@ -197,6 +231,8 @@ fun BenefitCard() {
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewMarketplaceUI() {
-    MarketplaceUI()
+private fun SearchPagePrev() {
+    MarketPlaceTheme {
+        SearchPage()
+    }
 }
