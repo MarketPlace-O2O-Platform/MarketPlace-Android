f1f402e7dc8fc5d3b0dd278b1c4c02e0381d3417
바키찬 <ckstmznf@naver.com>
2025-03-11 19:33:06 +0900

    CouponService member id 제거

--- app/src/main/java/dev/kichan/marketplace/model/repository/CouponRepository.kt
+++ app/src/main/java/dev/kichan/marketplace/model/repository/CouponRepository.kt
@@ -6,14 +6,14 @@ import dev.kichan.marketplace.model.service.CouponService
 class CouponRepository {
     private val service: CouponService = NetworkModule.getService(CouponService::class.java)
 
-    suspend fun getCouponList(memberId: Long, marketId: Long, lastCouponId: Long?, pageSize: Long?) =
-        service.getCouponList(memberId, marketId, lastCouponId, pageSize)
+    suspend fun getCouponList(marketId: Long, lastCouponId: Long?, pageSize: Long?) =
+        service.getCouponList(marketId, lastCouponId, pageSize)
 
-    suspend fun getPopularCoupon(memberId: Long, lastCouponId: Long?, pageSize: Long?) =
-        service.getPopularCoupon(memberId, lastCouponId, pageSize)
+    suspend fun getPopularCoupon(lastCouponId: Long?, pageSize: Long?) =
+        service.getPopularCoupon(lastCouponId, pageSize)
 
-    suspend fun getLatestCoupon(memberId: Long, lastCreatedAt: String?, lastCouponId: Long?, pageSize: Int) =
-        service.getLatestCoupon(memberId, lastCreatedAt, lastCouponId, pageSize)
+    suspend fun getLatestCoupon(lastCreatedAt: String?, lastCouponId: Long?, pageSize: Int) =
+        service.getLatestCoupon(lastCreatedAt, lastCouponId, pageSize)
 
     suspend fun getClosingCoupon(pageSize: Int) = service.getClosingCoupon(pageSize)
 }
