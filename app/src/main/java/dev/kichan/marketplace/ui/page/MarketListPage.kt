1a1b731afeb99c1442b146abbe7db96be6bc5c00
바키찬 <ckstmznf@naver.com>
2025-03-18 16:32:29 +0900

    Feat: 매장 리스트 보여주기

--- app/src/main/java/dev/kichan/marketplace/ui/page/MarketListPage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/MarketListPage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.github.javafaker.Bool
 import dev.kichan.marketplace.common.LargeCategory
 import dev.kichan.marketplace.model.NetworkModule
 import dev.kichan.marketplace.model.data.market.MarketRes
@@ -29,6 +31,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+data class Pagenation<T>(
+    val lastId: T?,
+    val hasNext: Boolean
+)
+
 @Composable
 fun MarketListPage(
     nacController: NavHostController = rememberNavController(),
@@ -36,28 +43,31 @@ fun MarketListPage(
 ) {
     val marketRepository = MarketRepository()
 
+    val listState = rememberLazyListState()
     var marketData by remember { mutableStateOf<List<MarketRes>>(listOf()) }
     var category by remember { mutableStateOf(_category) }
+    var pagenation by remember { mutableStateOf(Pagenation<Long>(null, true)) }
 
-    val onLoadData: (LargeCategory) -> Unit = {
+    val onLoadData: (LargeCategory, Boolean) -> Unit = {it, isInit ->
         CoroutineScope(Dispatchers.IO).launch {
             val res = marketRepository.getMarkets(
-                lastMarketId = null,
+                lastMarketId = if(isInit) null else pagenation.lastId.toString(),
                 category = if (it == LargeCategory.All) null else it.backendLable,
-                pageSize = 20
+                pageSize = 2002
             )
 
             withContext(Dispatchers.Main) {
                 if (res.isSuccessful) {
                     val body = res.body()!!.response.marketResDtos
                     marketData = body
+                    listState.scrollToItem(0)
                 }
             }
         }
     }
 
     LaunchedEffect(category) {
-        onLoadData(category)
+        onLoadData(category, true)
     }
 
     Scaffold {
@@ -66,7 +76,9 @@ fun MarketListPage(
         ) {
             CategoryTap(selectedCategory = category) { category = it }
 
-            LazyColumn {
+            LazyColumn(
+                state = listState
+            ) {
                 items(marketData) {market ->
                     MarketListItem(
                         title = market.name,
