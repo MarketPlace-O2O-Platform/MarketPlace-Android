450d0ca6eb1e24c146eddd12d6ce007dda311775
바키찬 <ckstmznf@naver.com>
2025-05-21 11:54:54 +0900

    Feat: HomePage에 Event를 Coupon으로 변경

---
+++ app/src/main/java/dev/kichan/marketplace/ui/component/atoms/CouponBox.kt
@@ -0,0 +1,100 @@
+package dev.kichan.marketplace.ui.component.atoms
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import dev.kichan.marketplace.R
+import dev.kichan.marketplace.ui.data.CouponBoxProps
+import dev.kichan.marketplace.ui.theme.PretendardFamily
+
+@Composable
+fun CouponBox(
+    modifier: Modifier = Modifier,
+    couponBoxProps: CouponBoxProps,
+) {
+    Box(
+        modifier = modifier
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(couponBoxProps.url)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Event Thumnail",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0x4D000000))
+        )
+
+        IconButton(
+            onClick = { couponBoxProps.onDownloadClick() },
+            modifier = Modifier.align(Alignment.TopEnd)
+        ) {
+            Icon(
+                painter = painterResource(if(couponBoxProps.isDownload) R.drawable.ic_bookmark_fill else R.drawable.ic_bookmark),
+                contentDescription = null
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(12.dp)
+        ) {
+            Text(
+                text = couponBoxProps.subTitle,
+                color = Color(0xffffffff),
+                fontFamily = PretendardFamily,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+            )
+            Text(
+                text = couponBoxProps.title,
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewEventBox() {
+    val couponBoxProps = CouponBoxProps(id = "ㅇ난영", title = "50% 할인권", subTitle = "싸다싸다", url = "image.kichan.dev/test.png", marketId = 1, onDownloadClick = {}, isDownload = false)
+    CouponBox(
+        modifier = Modifier
+            .fillMaxWidth(0.7f)
+            .aspectRatio(1f / 1),
+        couponBoxProps = couponBoxProps,
+    )
+}
---
+++ app/src/main/java/dev/kichan/marketplace/ui/component/molecules/CouponBoxList.kt
@@ -0,0 +1,82 @@
+package dev.kichan.marketplace.ui.component.molecules
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import dev.kichan.marketplace.ui.data.CouponBoxProps
+import dev.kichan.marketplace.ui.PAGE_HORIZONTAL_PADDING
+import dev.kichan.marketplace.ui.Page
+import dev.kichan.marketplace.ui.component.atoms.EmptyMessage
+import dev.kichan.marketplace.ui.component.atoms.CouponBox
+import dev.kichan.marketplace.ui.component.atoms.MoreViewTitle
+import dev.kichan.marketplace.ui.component.atoms.SkeletonItem
+
+@Composable
+fun CouponBoxList(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    isLoading: Boolean,
+    title: String,
+    couponList: List<CouponBoxProps>,
+    onMoreClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        MoreViewTitle(
+            title = title
+        ) {
+            onMoreClick()
+        }
+
+        if (isLoading) {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 7.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = PAGE_HORIZONTAL_PADDING)
+            ) {
+                items(5) {
+                    SkeletonItem(
+                        modifier = Modifier
+                            .fillParentMaxSize(0.8f)
+                            .aspectRatio(1f / 1),
+                    )
+                }
+            }
+        } else if (couponList.isEmpty()) {
+            EmptyMessage()
+        } else {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 7.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = PAGE_HORIZONTAL_PADDING)
+            ) {
+                items(couponList) {
+                    CouponBox(
+                        modifier = Modifier
+                            .clickable {
+                                navController.navigate("${Page.EventDetail.name}/${it.marketId}")
+                            }
+                            .fillParentMaxSize(0.8f)
+                            .aspectRatio(1f / 1),
+                        couponBoxProps = it,
+                    )
+                }
+            }
+        }
+    }
+}
