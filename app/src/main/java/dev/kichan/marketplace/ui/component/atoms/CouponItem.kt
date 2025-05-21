31e5debed06c55a330d9219c9956d66b87f73c6b
바키찬 <ckstmznf@naver.com>
2025-05-21 14:09:22 +0900

    Feat : 큐레이션 쿠폰

--- app/src/main/java/dev/kichan/marketplace/ui/component/atoms/CouponItem.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/component/atoms/CouponItem.kt
@@ -41,6 +41,7 @@ import dev.kichan.marketplace.model.NetworkModule
 import dev.kichan.marketplace.model.data.coupon.IssuedCouponRes
 import dev.kichan.marketplace.ui.faker
 import dev.kichan.marketplace.ui.theme.PretendardFamily
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 @Composable
@@ -53,13 +54,21 @@ fun CouponItem(
     val lightGray = Color(0xFFBFBFBF)
     val dashColor = Color.LightGray
 
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val parsedDate = LocalDate.parse(coupon.deadLine.substring(0, 10), formatter)
+    val expireDate =
+        parsedDate.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일까지"))
+
     Row(
         modifier = modifier
             .height(100.dp)
             .fillMaxWidth()
     ) {
         AsyncImage(
-            model = NetworkModule.getImageModel(LocalContext.current, NetworkModule.getImage(coupon.thumbnail)),
+            model = NetworkModule.getImageModel(
+                LocalContext.current,
+                NetworkModule.getImage(coupon.thumbnail)
+            ),
             contentDescription = null,
             modifier = Modifier
                 .width(100.dp)
@@ -71,30 +80,29 @@ fun CouponItem(
         Column(
             modifier = Modifier
                 .weight(1f)
+                .fillMaxHeight()
                 .background(Color(0xFFEFEFEF))
                 .padding(start = 16.dp, top = 12.dp, bottom = 12.dp),
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "70%",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold
+                text = coupon.couponName,
+                maxLines = 1,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = PretendardFamily,
             )
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = coupon.couponName,
-                    fontSize = 15.sp,
-                    lineHeight = 24.sp,
-                    fontFamily = PretendardFamily,
-                    fontWeight = FontWeight(600),
-                    color = Color(0xFF121212),
-                )
-                Icon(Icons.Rounded.KeyboardArrowRight, contentDescription = null)
-            }
+            Spacer(modifier = Modifier.height(12.dp))
             Text(
-                text = coupon.deadLine,
+                text = coupon.description ?: "",
+                maxLines = 1,
                 fontSize = 13.sp,
-                lineHeight = 22.sp,
+                fontFamily = PretendardFamily,
+            )
+            Text(
+                text = expireDate,
+                fontSize = 13.sp,
+                maxLines = 1,
                 fontFamily = PretendardFamily,
                 fontWeight = FontWeight(400),
                 color = Color(0xFF545454),
@@ -105,7 +113,8 @@ fun CouponItem(
             modifier = Modifier
                 .width(72.dp)
                 .fillMaxHeight()
-                .background(if(type == "ISSUED") darkGrey else lightGray)
+                .background(if (type == "ISSUED") darkGrey else lightGray),
+            contentAlignment = Alignment.Center
         ) {
             Canvas(modifier = Modifier.matchParentSize()) {
                 val dash = 8.dp.toPx()
@@ -125,16 +134,13 @@ fun CouponItem(
                 drawCircle(Color.White, radius = r, center = Offset(0f, size.height))
             }
             Text(
-                text = if(type == "ISSUED") "사용 가능" else if(type == "USED") "사용 완료" else "기간 만료",
+                text = if (type == "ISSUED") "사용 가능" else if (type == "USED") "사용 완료" else "기간 만료",
                 fontSize = 13.sp,
                 lineHeight = 30.sp,
                 fontFamily = PretendardFamily,
                 fontWeight = FontWeight(700),
                 color = Color(0xFFFFFFFF),
                 letterSpacing = 0.26.sp,
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(end = 20.dp)
             )
         }
     }
