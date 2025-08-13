65044ee50752262bdbc55bddff6add9da0df6a50
바키찬 <ckstmznf@naver.com>
2025-08-13 17:40:20 +0900

    Feat: 사용자 데이터 조회 수정

--- app/src/main/java/dev/kichan/marketplace/model/data/login/MemberLoginReq.kt
+++ app/src/main/java/dev/kichan/marketplace/model/data/login/MemberLoginReq.kt
@@ -1,10 +1,8 @@
 package dev.kichan.marketplace.model.data.login
 
-data class LoginRes(
-    val studentId : Long,
-)
-
 data class MemberLoginRes(
     val studentId : Long,
-    val cheerTicket: Int
+    val cheerTicket: Int,
+    val account: String,
+    val accountNumber: String,
 )
