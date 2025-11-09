54f2b73f7777435615784a7ed9256ef545c4a8e0
Junhee-lee <sk5514224@gmail.com>
2025-11-10 00:19:06 +0900

    fix: 환급하기 페이지 하단 버튼이 시스템 네비게이션 바에 가려지는 문제 해결

--- app/src/main/java/dev/kichan/marketplace/ui/page/ReceiptUploadPage.kt
+++ app/src/main/java/dev/kichan/marketplace/ui/page/ReceiptUploadPage.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -67,6 +70,7 @@ fun ReceiptUploadPage(
             modifier = modifier
                 .padding(it)
                 .padding(20.dp)
+                .windowInsetsPadding(WindowInsets.navigationBars)
         ) {
             Text(
                 "환급 받을 영수증을\n" + "등록해주세요",
