34ddff759523da32f5242d595adc39f2646af9fb
Junhee-lee <sk5514224@gmail.com>
2025-11-22 18:10:06 +0900

    chore: 버전 2.0.0으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "dev.kichan.marketplace"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
