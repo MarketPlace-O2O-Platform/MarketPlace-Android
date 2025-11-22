3eab2a250cde3f02e24f6277a985bd3b7ccb1c0f
Junhee-lee <sk5514224@gmail.com>
2025-11-22 18:34:57 +0900

    chore: 버전 코드만 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "dev.kichan.marketplace"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
